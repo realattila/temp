706aa5b61d534f258449dbf07825ba2c34f4d6f8
attila <98atila@gmail.com>
2021-10-18 11:13:30 +0330

    add Layout support in app & fix some styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,19 +7,25 @@ import { Provider } from 'react-redux';
 import { store } from 'store/index';
 import { appWithTranslation } from 'next-i18next';
 import NextNProgress from 'nextjs-progressbar';
-import { withAuth } from 'services/auth-service';
 import { useEffect } from 'react';
+import { Page } from 'types/page';
 
-function MyApp({ Component, pageProps }: AppProps) {
+type Props = AppProps & {
+  Component: Page;
+};
+
+function MyApp({ Component, pageProps }: Props) {
   useEffect(() => {
     document.dir = 'rtl';
   }, []);
+
+  const getLayout = Component.getLayout || ((page) => page);
+
   return (
     <Provider store={store}>
       <NextNProgress color='#00b67a' startPosition={0.3} stopDelayMs={200} height={2} showOnShallow={true} />
-
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
     </Provider>
   );
 }
-export default appWithTranslation(withAuth(MyApp));
+export default appWithTranslation(MyApp);
