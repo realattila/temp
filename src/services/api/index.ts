3a6ecb6e05cbf110cb135ccca3bfc08c3aed9f79
attila <98atila@gmail.com>
2021-10-23 11:31:10 +0330

    change API instance logic

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -80,18 +80,20 @@ export interface AxiosInstance {
 
 export const axiosApi = axios.create({ baseURL: process.env.API_BASE_URL, timeout: 3000 });
 
+const SERVER_ERROR = 'خطای سرور رخ داده است مجدد تلاش کنید';
+
 axiosApi.interceptors.response.use(
   (response) => {
     return { ...response, hasError: false, errorText: '' };
   },
   (error: AxiosError) => {
     if (error.code === 'ECONNABORTED') {
-      return { ...error, hasError: true, statue: 500, errorText: error.request?.response || true };
+      return { ...error, hasError: true, statue: 500, errorText: error.request?.response || SERVER_ERROR };
     }
     return {
       ...error,
       hasError: true,
-      errorText: error.request?.response || true,
+      errorText: error.request?.response || SERVER_ERROR,
       errorStatus: error.request?.status || 500,
     };
   },
@@ -99,47 +101,64 @@ axiosApi.interceptors.response.use(
 
 // const API = async (props: AxiosRequestConfig, withAuth: boolean = false) => {
 const API = async ({ withAuth = false, ...props }) => {
-  try {
-    let headers = props.headers;
-    if (withAuth) {
-      const token = window.localStorage.getItem('token');
-      headers = { ...headers, Authorization: `Bearer ${token}` };
-    }
-    const res = await axiosApi({ ...props, headers: headers });
-    if ((res.status === 401 || res.request.status === 401) && withAuth) {
-      return await repeatReuest(res.config);
-    }
+  // try {
+  //   let headers = props.headers;
+  //   if (withAuth) {
+  //     const token = window.localStorage.getItem('token');
+  //     headers = { ...headers, Authorization: `Bearer ${token}` };
+  //   }
+  //   const res = await axiosApi({ ...props, headers: headers });
+  //   if ((res.status === 401 || res.request.status === 401) && withAuth) {
+  //     return await repeatReuest(res.config);
+  //   }
 
-    return res;
-  } catch (e: any) {
-    if ((e.status === 401 || e?.request?.status === 401 || e?.response?.statue === 401) && withAuth) {
-      return await repeatReuest(e.config);
-    }
-    return e;
+  //   return res;
+  // } catch (e: any) {
+  //   if ((e.status === 401 || e?.request?.status === 401 || e?.response?.statue === 401) && withAuth) {
+  //     return await repeatReuest(e.config);
+  //   }
+  //   return e;
+  // }
+
+  let headers = props.headers;
+  if (withAuth) {
+    const token = window.localStorage.getItem('token');
+    headers = { ...headers, Authorization: `Bearer ${token}` };
   }
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axiosApi({ ...props, headers: headers });
+      if ((res.status === 401 || res.request.status === 401) && withAuth) {
+        await repeatReuest(res.config, resolve, reject);
+      }
+      resolve(res);
+    } catch (e: any) {
+      if ((e.status === 401 || e?.request?.status === 401 || e?.response?.statue === 401) && withAuth) {
+        await repeatReuest(e.config, resolve, reject);
+      }
+      reject(e);
+    }
+  });
 };
 
-const repeatReuest = async (_config: any) => {
+const repeatReuest = async (_config: any, resolve: (value: unknown) => void, reject: (reason?: any) => void) => {
   const AuthServiceInstance = new AuthService();
   return await AuthServiceInstance.renewToken({ state: window.location.href })
     .then(async (user) => {
-      console.log('user', user);
       window.localStorage.setItem('token', user?.access_token);
       const res = await axiosApi({
         ..._config,
         headers: { ..._config.headers, Authorization: `Bearer ${user?.access_token}` },
       });
-
       if (res.status === 401 || res.request.status === 401) {
         AuthServiceInstance.login();
-        return res;
+        return reject(res);
       }
-
-      return res;
+      return resolve(res);
     })
     .catch((e) => {
       AuthServiceInstance.login();
-      return e;
+      return reject({ status: 500, hasError: true, errorText: SERVER_ERROR, errorStatus: 500 });
     });
 };
 
